27d4d054444f39b90e9f6ea2f6de47255253a142
Nathalie Marquesin <nathaliemarquesin@gmail.com>
2017-08-14 13:05:37 +0100

    After lunch save

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+class Test extends Component() {
+  render() {
+    return(
+      <h1>Hello from the other side</h1>
+    )
+  }
+}
+
+export default Test
